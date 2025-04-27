f7bc32d1d9aac5a2665fafde09c51aae08f06c92
rafawolver <tvforasta@gmail.com>
2025-04-27 11:50:31 -0300

    Respondendo os Exercicios
    update

--- Aulas_Guanabara/Exercicios/Exercicio_06.py
+++ Aulas_Guanabara/Exercicios/Exercicio_06.py
@@ -1,1 +1,7 @@
 # Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+
+n1 = int(input('Me diga um valor: '))
+n2 = n1*2
+n3 = n1*3
+n4 = n1**(1/2)
+print('Em relação ao numero {}, posso lhe informar que: \n Seu dobro é: {} \n Seu triplo é: {} \n E sua raiz quadrada é: {}'. format(n1, n2, n3, n4))
--- Aulas_Guanabara/Exercicios/Exercicio_07.py
+++ Aulas_Guanabara/Exercicios/Exercicio_07.py
@@ -1,1 +1,7 @@
 # Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+
+n1 = int(input('Me diga qual nota você tirou em Matemática: '))
+n2 = int(input('Me diga qual nota você tirou em Portugues: '))
+s = n1+n2
+r = s/2
+print('A sua media de notas em relação a Matemática e Portugues é {}!'.format(r))
